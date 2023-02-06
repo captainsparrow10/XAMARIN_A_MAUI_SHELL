8086346fadeb97cf472c7ecdbaf931eb49e00bae
captainsparrow10 <javier1009rm@gmial.com>
2023-02-06 09:59:38 -0500

    error de subida de archivos

--- PlantLady/PlantLady.Android/MainActivity.cs
+++ PlantLady/PlantLady.Android/MainActivity.cs
@@ -14,8 +14,20 @@ namespace PlantLady.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(savedInstanceState);
-        }
+            //TabLayoutResource = Resource.Layout.Tabbar;
+            //ToolbarResource = Resource.Layout.Toolbar;
 
+            base.OnCreate(savedInstanceState);
+
+            //Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            //global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            //LoadApplication(new App());
+        }
+        //public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        //{
+        //    Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        //    base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        //}
     }
 }
--- PlantLady/PlantLady.iOS/AppDelegate.cs
+++ PlantLady/PlantLady.iOS/AppDelegate.cs
@@ -7,7 +7,9 @@ using UIKit;
 
 namespace PlantLady.iOS
 {
-
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
     [Register("AppDelegate")]
     public partial class AppDelegate : MauiUIApplicationDelegate
     {
